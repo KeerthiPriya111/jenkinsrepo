d7ad35232cdde82b04403644517bc421286cb872
KeerthiPriya1116 <pkeerthi.priya1602@gmail.com>
2024-07-31 10:47:40 +0530

    UPDATE

--- AAA/src/test/java/aa/Demo_Test.java
+++ AAA/src/test/java/aa/Demo_Test.java
@@ -6,6 +6,16 @@ public class Demo_Test {
 	@Test
 	public void printTest() {
 		System.out.println("hello");
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String UN = System.getProperty("username");
+		String pwd = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(UN);
+		System.out.println(pwd);
+		
 	}
 	
 	@Test
