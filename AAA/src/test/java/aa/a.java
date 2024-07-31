6f77578262ebaf6ba26835ee4bb050ecd0e8ad5e
KeerthiPriya1116 <pkeerthi.priya1602@gmail.com>
2024-07-31 09:37:10 +0530

    added sample project

---
+++ AAA/src/test/java/aa/a.java
@@ -0,0 +1,15 @@
+package aa;
+
+import org.testng.annotations.Test;
+
+public class a {
+	@Test
+	public void test() {
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void Create_contact() {
+		System.out.println("execute contact test");
+	}
+}
---
+++ AAA/src/test/java/bb/b.java
@@ -0,0 +1,11 @@
+package bb;
+
+import org.testng.annotations.Test;
+
+public class b {
+
+	@Test
+	public void orgtest() {
+		System.out.println("execute org test");
+	}
+}
